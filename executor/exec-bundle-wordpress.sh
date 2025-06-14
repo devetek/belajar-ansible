c5b3b0437124bf407c4704d11b6b6cbbe74a529a
Prakasa <prakasa@devetek.com>
2025-06-14 15:55:39 +0700

    feat: add new playbook to create laravel app with composer (#5)

--- executor/exec-bundle-wordpress.sh
+++ executor/exec-bundle-wordpress.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
 export SLACK_WEBHOOK_URL="https://hooks.slack.com/services/fake-app/fake-group/fake-id"
-export SLACK_CHANNEL="#dpanel-resource"
-export SLACK_USERNAME="dPanel-Creator"
 
 go run main.go -i /ansible/inventory/ansible-inventory.ini -p /ansible/playbooks/bundle-wordpress.yml -u root -k /executor/id_rsa_fake -t all -e @/executor/variables/bundle-wordpress.json
