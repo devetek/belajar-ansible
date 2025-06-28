0b0bf7c16ee9784fbc70ad118bebb616f1b74c87
Prakasa <prakasa@devetek.com>
2025-06-28 16:16:39 +0700

    fix: disable extra plugins

--- executor/exec-module-emqx.sh
+++ executor/exec-module-emqx.sh
@@ -2,4 +2,7 @@
 
 export SLACK_WEBHOOK_URL="https://hooks.slack.com/services/fake-app/fake-group/fake-id"
 
+# Development
 go run main.go -i /ansible/inventory/ansible-inventory.ini -p /ansible/playbooks/module-emqx.yml -u root -k /executor/id_rsa_fake -t all
+# Production
+# go run main.go -i /ansible/inventory/ansible-inventory-prod.ini -p /ansible/playbooks/module-emqx.yml -u root -k /executor/id_rsa_prod -t all
