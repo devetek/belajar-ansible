fb6ab7970e923b503cd584bd6c7ed317167a3a53
Prakasa <prakasa@devetek.com>
2025-07-30 16:51:25 +0700

    fix: add more example how dPanel works under the hood (#7)
    * fix: add logrotate role
    
    * fix: add more example how dPanel works under the hood

---
+++ executor/exec-module-mongodb.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+export SLACK_WEBHOOK_URL="https://hooks.slack.com/services/fake-app/fake-group/fake-id"
+
+go run main.go -i /ansible/inventory/ansible-inventory.ini -p /ansible/playbooks/module-mongodb.yml -u root -k /executor/id_rsa_fake -t all -e @/executor/variables/module-mongodb.json
