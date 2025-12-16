24760a5a41c2e487ac045e1a154d0bd7e1a634e8
Prakasa <prakasa@devetek.com>
2025-12-16 10:43:45 +0700

    fix: add command complete destory

--- executor/exec-kolla-destory.sh
+++ executor/exec-kolla-destory.sh
@@ -3,3 +3,7 @@
 export KOLLA_CONFIG_PATH="ansible/inventory/group_vars/kolla" 
 
 kolla-ansible destroy -i ansible/inventory/kolla/all-in-one
+
+rm -rf /etc/kolla /var/lib/kolla
+
+docker system prune -af
